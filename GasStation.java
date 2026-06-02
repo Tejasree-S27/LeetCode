fda4b0b88652272fb757f64dca713ee7a1a06c94
Saligari Tejasree <stejasree348@gmail.com>
2026-06-02 16:24:41 +0530

    Implement gas station circuit solution

---
+++ GasStation.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0;
+        int totalCost = 0;
+        int tank = 0;
+        int start = 0;
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+            tank += gas[i] - cost[i];
+            // If tank becomes negative, current start is invalid
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        // If total gas is less than total cost, completing the circuit is impossible
+        return totalGas >= totalCost ? start : -1;
+    }
+}
