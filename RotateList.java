2504d697f9632de8cdf75408c00de8c120dcac7f
Saligari Tejasree <stejasree348@gmail.com>
2026-06-13 20:09:58 +0530

    Create  RotateList.java

---
+++ RotateList.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null || head.next==null || k==0) {
+            return head;
+        }
+        int length=1;
+        ListNode tail=head;
+        while (tail.next!=null) {
+            tail=tail.next;
+            length++;
+        }
+        k=k%length;
+        if(k==0) {
+            return head;
+        }
+        // Make circular list
+        tail.next=head;
+        int stepsToNewTail=length-k-1;
+        ListNode newTail=head;
+        for(int i=0;i<stepsToNewTail;i++) {
+            newTail=newTail.next;
+        }
+        ListNode newHead=newTail.next;
+        newTail.next=null;
+        return newHead;
+    }
+}
