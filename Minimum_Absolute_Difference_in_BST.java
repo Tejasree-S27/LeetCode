250ca168ffb28544b78ee4a056e00eb0673d67a0
Saligari Tejasree <stejasree348@gmail.com>
2026-08-17 18:39:58 +0530

    Create Minimum_Absolute_Difference_in_BST.java

---
+++ Minimum_Absolute_Difference_in_BST.java
@@ -0,0 +1,18 @@
+class Solution {
+    private int minDiff=Integer.MAX_VALUE;
+    private Integer prev=null;
+    public int getMinimumDifference(TreeNode root) {
+        inorder(root);
+        return minDiff;
+    }
+    private void inorder(TreeNode node) {
+        if(node==null) 
+        return;
+        inorder(node.left);
+        if(prev!=null) {
+            minDiff=Math.min(minDiff,node.val-prev);
+        }
+        prev=node.val;
+        inorder(node.right);
+    }
+}
