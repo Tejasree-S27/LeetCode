845bd3d063c9f2f6c3ad91235d0dcc9bee4e070f
Saligari Tejasree <stejasree348@gmail.com>
2026-08-17 18:27:31 +0530

    Create MeanOfArrayAfterRemovingSomeElements.java

---
+++ MeanOfArrayAfterRemovingSomeElements.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public double trimMean(int[] arr) {
+        Arrays.sort(arr);
+        int n=arr.length;
+        int remove=n/20;
+        int sum=0;
+        for(int i=remove;i<n-remove;i++) 
+            sum+=arr[i];
+        return (double)sum/(n-2*remove);
+    }
+}
