0494488517b07603029fbed5d43cc20d2f4fa88e
Saligari Tejasree <stejasree348@gmail.com>
2026-08-26 18:16:48 +0530

    Create Shortest_and_Lexicographically_Smallest_Beautiful_String.java

---
+++ Shortest_and_Lexicographically_Smallest_Beautiful_String.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+        int left=0;
+        int ones=0;
+        String answer="";
+        for(int right=0;right<s.length();right++) {
+            if(s.charAt(right)=='1') {
+                ones++;
+            }
+            // If we have more than k ones, shrink the window
+            while(ones>k) {
+                if(s.charAt(left)=='1') {
+                    ones--;
+                }
+                left++;
+            }
+            // Remove unnecessary leading zeros
+            while(ones==k && s.charAt(left)=='0') {
+                left++;
+            }
+            // We found a beautiful substring
+            if(ones==k) {
+                String current=s.substring(left,right+1);
+                if(answer.isEmpty() || current.length()<answer.length() || (current.length()==answer.length() && current.compareTo(answer)<0)) {
+                    answer=current;
+                }
+            }
+        }
+        return answer;
+    }
+}
