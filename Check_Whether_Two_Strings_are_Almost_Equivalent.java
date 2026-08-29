c627bb889266fca94dfd2e9e588884e130b26110
Saligari Tejasree <stejasree348@gmail.com>
2026-08-29 19:40:06 +0530

    Create Check_Whether_Two_Strings_are_Almost_Equivalent.java

---
+++ Check_Whether_Two_Strings_are_Almost_Equivalent.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        int[] a=new int[26];
+        int[] b=new int[26];
+        for(char c : word1.toCharArray()) a[c-'a']++;
+        for(char c : word2.toCharArray()) b[c-'a']++;
+        for(int i=0;i<26;i++)
+            if(Math.abs(a[i]-b[i])>3)
+                return false;
+        return true;
+    }
+}
