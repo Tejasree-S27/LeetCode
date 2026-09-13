3e24b86edcec83a36e4849cf5f7fc6f33770385f
Saligari Tejasree <stejasree348@gmail.com>
2026-09-13 21:13:58 +0530

    Create Number_Complement.java

---
+++ Number_Complement.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findComplement(int num) {
+        int mask = 0;
+        int temp = num;
+        // Create mask: 111...111
+        while (temp > 0) {
+            mask = (mask << 1) | 1;
+            temp >>= 1;
+        }
+        // Flip only the meaningful bits
+        return num ^ mask;
+    }
+}
