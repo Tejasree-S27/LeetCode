a49046f4a61a7bfba01bbfe74c8646064db3b189
Saligari Tejasree <stejasree348@gmail.com>
2026-09-23 19:13:08 +0530

    Create Binary_Tree_Tilt.java

---
+++ Binary_Tree_Tilt.java
@@ -0,0 +1,16 @@
+class Solution {
+    int totalTilt = 0;
+    public int findTilt(TreeNode root) {
+        sum(root);
+        return totalTilt;
+    }
+    private int sum(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftSum = sum(node.left);
+        int rightSum = sum(node.right);
+        totalTilt += Math.abs(leftSum - rightSum);
+        return node.val + leftSum + rightSum;
+    }
+}
